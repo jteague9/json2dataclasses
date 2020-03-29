37cd2159f2837993b431c594dfade1578796329c
jason <jteague9@gatech.edu>
2020-03-29 00:41:33 -0700

    Init commit

---
+++ main.py
@@ -0,0 +1,232 @@
+import argparse
+import autopep8
+import json
+from dataclasses import dataclass, field
+from typing import Any, Union
+import keyword
+
+keywords = [word.lower() for word in keyword.kwlist]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Build dataclasses from example json.')
+    parser.add_argument('--name', '-n', help='name of structure')
+    parser.add_argument('--filename', '-f', help='filename of input')
+    parser.add_argument('--text', '-t', help='text input')
+    parser.add_argument('--output', '-o', help='output destination file')
+    args = parser.parse_args()
+
+    filename = args.filename
+    text = args.text
+    name = args.name
+    output = args.output
+
+    data = None
+    if filename:
+        with open(filename) as file:
+            data = json.load(file)
+    elif text:
+        data = json.loads(text)
+    else:
+        print("enter filename or text")
+        exit(1)
+
+    if name is None:
+        print("include a name")
+        exit(1)
+
+    res = build_dataclass_from_data(name, data)
+    res = autopep8.fix_code(res)
+    with open(output, 'w') as file:
+        print(file.write(res))
+
+
+@dataclass
+class Node:
+    parent: Union['Node', None]
+    data: Any
+    type: str
+    subtype: str = None
+    name: str = None
+    subnodes: list = field(default_factory=list)
+    type_: str = field(init=False, repr=False)
+
+    @property
+    def type(self) -> str:
+        return self.type_
+
+    @type.setter
+    def type(self, v: str):
+        self.type_ = v if v.lower() not in keywords else v + "_"
+
+
+def node_generator(node, head_first=False):
+    if head_first:
+        yield node
+    for s in node.subnodes:
+        for n in node_generator(s, head_first=head_first):
+            yield n
+    if not head_first:
+        yield node
+
+
+def get_base_node(name, data):
+    if type(data) is list:
+        type_ = "list"
+    else:
+        type_ = name.capitalize()
+
+    node = Node(None, type=type_, data=data, subtype=name)
+    return node
+
+
+def build_tree(node):
+    type_ = type(node.data).__name__
+    if type_ == "list":
+        if len(node.data) == 0:
+            new_node = Node(node, type="Any", data=None)
+            node.subtype = "Any"
+            node.subnodes.append(new_node)
+        else:
+            value = node.data[0]
+            new_type = type(value).__name__
+            if new_type == "dict":
+                if node.name is not None:
+                    new_type = node.name.capitalize()
+                    if new_type[-2:] == 'es':  # infer a singular object name from plural list name
+                        new_type = new_type[:-2]
+                    if new_type[-1:] == 's':
+                        new_type = new_type[:-1]
+                else:
+                    new_type = node.subtype.capitalize()
+            new_node = Node(node, type=new_type, data=value)
+            node.subtype = new_type
+            node.subnodes.append(new_node)
+            build_tree(new_node)
+    elif type_ == "dict":
+        for key, value in node.data.items():
+            new_type = type(value).__name__
+            if new_type == "dict":
+                new_type = key.capitalize()
+            new_node = Node(node, type=new_type, name=key, data=value)
+            node.subnodes.append(new_node)
+            build_tree(new_node)
+
+
+def get_node_class(node):
+    content = "@dataclass\n"
+    content += f"class {node.type}:\n"
+    for n in node.subnodes:
+        if n.type == "NoneType":
+            content += f"\t{n.name}: Any = None\n"
+        elif n.type == "list":
+            content += f"\t{n.name}: List[{n.subtype}] = field(default_factory=list)\n"
+        else:
+            content += f"\t{n.name}: {n.type} = None\n"
+    if len(node.subnodes) == 0:
+        content += "\tpass\n"
+    return content + '\n\n'
+
+
+def get_node_classes_list(node):
+    content = list()
+    for s_node in node.subnodes:
+        content += get_node_classes_list(s_node)
+    if node.type not in ['int', 'bool', 'float', 'str', 'NoneType', 'list', 'Any']:
+        content.append(get_node_class(node))
+    return content
+
+
+def get_node_classes(node):
+    class_list = get_node_classes_list(node)
+    unique_list = list()
+    for class_ in class_list:
+        if class_ not in unique_list:
+            unique_list.append(class_)
+    return ''.join(unique_list)
+
+
+def get_basic_node_definition(node, var=None):
+    content = f".get('{node.name}')"
+    parent = node.parent
+    while parent is not None and parent.type != "list":
+        if parent.name:
+            content = f".get('{parent.name}', {{}})" + content
+        parent = parent.parent
+    if parent is None:
+        var = var or "response"
+        content = var + content
+    elif parent.type == "list":
+        var = var
+        content = var + content
+    return content
+
+
+def get_object_node_definition(node, var=None):
+    content = f"{node.type}("
+    for s in node.subnodes:
+        content += f"{s.name}={get_node_definition(s, var)},"
+    content += ")"
+    return content
+
+
+def get_list_node_definition(node, var=None):
+    nvar = node.subtype.lower()
+    if node.parent:
+        baselist = f".get('{node.name}', [])"
+    else:
+        baselist = ""
+    parent = node.parent
+    while parent is not None and parent.type != "list":
+        if parent.name:
+            baselist = f".get('{parent.name}', {{}})" + baselist
+        parent = parent.parent
+    if parent is None:
+        baselist = "response" + baselist
+    elif parent.type == "list":
+        baselist = var + baselist
+    content = f"[{get_node_definition(node.subnodes[0], nvar)} for {nvar} in {baselist}]"
+    return content
+
+
+def get_node_definition(node, var=None):
+    if node.type in ['str', 'int', 'float', 'bool', 'NoneType']:
+        return get_basic_node_definition(node, var)
+    elif node.type == "list":
+        return get_list_node_definition(node, var)
+    elif node.type == "Any":  # special case for a json file containing an empty list we can't infer about
+        return "any"
+    else:
+        return get_object_node_definition(node, var)
+
+
+def build_dataclass_from_data(name, data):
+    base_node = get_base_node(name, data)
+    build_tree(base_node)
+    specificy_duplicate_types(base_node)
+    models = get_node_classes(base_node)
+    translate_line = get_node_definition(base_node)
+    translate_func = f"def translate(response):\n\treturn {translate_line}\n"
+    header = "from dataclasses import dataclass, field\nfrom typing import List"
+    if "Any" in models:
+        header += ", Any"
+    header += '\n\n\n'
+    result = header + models + translate_func
+    return result
+
+
+def specificy_duplicate_types(base_node):
+    type_list = [node.type for node in node_generator(base_node)]
+    basic_types = ['str', 'int', 'float', 'bool', 'NoneType', 'Any', 'list']
+    dup_types = [type_ for type_ in type_list if type_ not in basic_types and type_list.count(type_) > 1]
+    for n in node_generator(base_node):
+        if n.type in dup_types and n.parent is not None:
+            if n.parent.type == "list":
+                n.type = f"{n.parent.subtype.capitalize()}{n.type.capitalize()}"
+                n.parent.subtype = n.type
+            else:
+                n.type = f"{n.parent.type.capitalize()}{n.type.capitalize()}"
+
+
+if __name__ == "__main__":
+    main()
---
+++ test/testmain.py
@@ -0,0 +1,236 @@
+from importlib.util import spec_from_file_location, module_from_spec
+import json
+import keyword
+import main
+import os
+import pytest
+
+
+@pytest.fixture
+def github_json():
+    with open('github.json') as file:
+        m = json.load(file)
+    return m
+
+
+@pytest.fixture
+def articles_json():
+    with open('articles.json') as file:
+        m = json.load(file)
+    return m
+
+
+@pytest.fixture
+def errors_json():
+    with open('errors.json') as file:
+        m = json.load(file)
+    return m
+
+
+@pytest.fixture
+def name():
+    return "Test"
+
+
+@pytest.fixture
+def data_dict():
+    return {"field1": {'field2': {"field3": "fin"}}}
+
+
+@pytest.fixture
+def data_list():
+    return [{"field1": {'field2': {"field3": "fin1"}}}, {"field1": {'field2': {"field3": "fin2"}}}]
+
+
+@pytest.fixture
+def empty_list_dict():
+    return {"members": []}
+
+
+def test_get_base_node_dict(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    assert node.data == data_dict
+    assert node.type == name.capitalize()
+
+
+def test_get_base_node_list(name, data_list):
+    node = main.get_base_node(name, data_list)
+    assert node.data == data_list
+    assert node.type == "list"
+    assert node.subtype == name.capitalize()
+
+
+def test_get_base_node_empty_list_dict(name, empty_list_dict):
+    node = main.get_base_node(name, empty_list_dict)
+    assert node.data == empty_list_dict
+    assert node.type == name.capitalize()
+
+
+def test_get_base_node_reserved_name(data_dict):
+    for reserved_name in keyword.kwlist:
+        node = main.get_base_node(reserved_name, data_dict)
+        assert node.data == data_dict
+        assert node.type != reserved_name.capitalize()
+
+
+def test_node_generator(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    names = ['str', 'Field2', 'Field1', 'Test']
+    count = len(names)
+    for n in main.node_generator(node, head_first=False):
+        assert n.type == names.pop(0)
+        count -= 1
+    assert count == 0
+
+
+def test_node_generator_reverse(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    names = ['Test', 'Field1', 'Field2', 'str']
+    count = len(names)
+    for n in main.node_generator(node, head_first=True):
+        assert n.type == names.pop(0)
+        count -= 1
+    assert count == 0
+
+
+def test_build_tree_dict(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    s_node = node.subnodes[0].subnodes[0].subnodes[0]
+    assert s_node.name == "field3"
+    assert s_node.parent.name == "field2"
+    assert s_node.parent.parent.name == "field1"
+    assert s_node.parent.parent.parent.parent is None
+    assert s_node.data == "fin"
+
+
+def test_build_tree_list(name, data_list):
+    node = main.get_base_node(name, data_list)
+    main.build_tree(node)
+    s_node = node.subnodes[0].subnodes[0].subnodes[0].subnodes[0]
+    assert s_node.name == "field3"
+    assert s_node.parent.name == "field2"
+    assert s_node.parent.parent.name == "field1"
+    assert s_node.parent.parent.parent.parent.parent is None
+    assert s_node.data == "fin1"
+
+
+def test_build_tree_empty_list_dict(name, empty_list_dict):
+    node = main.get_base_node(name, empty_list_dict)
+    main.build_tree(node)
+    assert node.subnodes[0].name == "members"
+
+
+def test_get_node_class(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    s_node = node.subnodes[0].subnodes[0]
+    assert main.get_node_class(s_node) == "@dataclass\nclass Field2:\n\tfield3: str = None\n\n\n"
+    assert main.get_node_class(s_node.parent) == "@dataclass\nclass Field1:\n\tfield2: Field2 = None\n\n\n"
+
+
+def test_get_node_classes(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    expected = "@dataclass\nclass Field2:\n\tfield3: str = None\n\n\n" \
+               "@dataclass\nclass Field1:\n\tfield2: Field2 = None\n\n\n" \
+               "@dataclass\nclass Test:\n\tfield1: Field1 = None\n\n\n"
+    assert main.get_node_classes(node) == expected
+
+
+def test_get_node_classes_empty_list_dict(name, empty_list_dict):
+    node = main.get_base_node(name, empty_list_dict)
+    main.build_tree(node)
+    assert main.get_node_classes(node) == "@dataclass\nclass Test:\n\tmembers: List[Any] = " \
+                                          "field(default_factory=list)\n\n\n"
+
+
+def test_get_node_definition(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    assert main.get_node_definition(node) == "Test(field1=Field1(field2=Field2(field3=response.get('field1', " \
+                                             "{}).get('field2', {}).get('field3'),),),)"
+
+
+def test_get_node_definition_empty_list_dict(name, empty_list_dict):
+    node = main.get_base_node(name, empty_list_dict)
+    main.build_tree(node)
+    assert main.get_node_definition(node) == "Test(members=[any for any in response.get('members', [])],)"
+
+
+def test_get_object_node(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    s_node = node.subnodes[0]
+    assert main.get_object_node_definition(s_node) == "Field1(field2=Field2(field3=response.get('field1', " \
+                                                      "{}).get('field2', {}).get('field3'),),)"
+
+
+def test_get_list_node(name, data_list):
+    node = main.get_base_node(name, data_list)
+    main.build_tree(node)
+    assert main.get_list_node_definition(node) == "[Test(field1=Field1(field2=Field2(field3=test.get('field1', " \
+                                                  "{}).get('field2', {}).get('field3'),),),) for test in response]"
+
+
+def test_get_basic_node(name, data_dict):
+    node = main.get_base_node(name, data_dict)
+    main.build_tree(node)
+    s_node = node.subnodes[0].subnodes[0].subnodes[0]
+    assert main.get_basic_node_definition(s_node) == "response.get('field1', {}).get('field2', {}).get('field3')"
+
+
+@pytest.mark.slow
+def test_build_translate_example(articles_json, tmp_path):
+    final = main.build_dataclass_from_data("Example", articles_json)
+    module_name = "Articles"
+    d = tmp_path / (module_name + ".py")
+    d.write_text(final)
+    spec = spec_from_file_location(module_name, d)
+    out = module_from_spec(spec)
+    spec.loader.exec_module(out)
+    example = out.translate(articles_json)
+    assert example.data[0].type == articles_json['data'][0]['type']
+
+
+@pytest.mark.slow
+def test_build_translate_example(github_json, tmp_path):
+    final = main.build_dataclass_from_data("EventList", github_json)
+    module_name = "eventlist"
+    d = tmp_path / (module_name + ".py")
+    d.write_text(final)
+    spec = spec_from_file_location(module_name, d)
+    out = module_from_spec(spec)
+    spec.loader.exec_module(out)
+    eventlist = out.translate(articles_json)
+    assert eventlist[0].type == github_json[0]['type']
+    assert eventlist[1].actor.id == github_json[1]['actor']['id']
+
+
+@pytest.mark.slow
+def test_build_translate_example(errors_json, tmp_path):
+    final = main.build_dataclass_from_data("Example", errors_json)
+    module_name = "Errors"
+    d = tmp_path / (module_name + ".py")
+    d.write_text(final)
+    spec = spec_from_file_location(module_name, d)
+    out = module_from_spec(spec)
+    spec.loader.exec_module(out)
+    example = out.translate(errors_json)
+    assert example.errors[0].status == errors_json['errors'][0]['status']
+    assert example.errors[1].source.pointer == errors_json['errors'][1]['source']['pointer']
+
+
+@pytest.mark.slow
+def test_cli_run(tmp_path, articles_json):
+    module_name = "Articles"
+    fileloc = tmp_path / (module_name + ".py")
+    res = os.system(f"python ../main.py -f articles.json -n {module_name} -o {fileloc}")
+    assert res == 0
+    spec = spec_from_file_location(module_name, fileloc)
+    out = module_from_spec(spec)
+    spec.loader.exec_module(out)
+    articles = out.translate(articles_json)
+    assert articles.data[0].type == articles_json['data'][0]['type']
